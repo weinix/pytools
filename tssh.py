d575c5b87e93c116a30a99b103aebb1f33cf7eb2
Wei Wang <wei.w.wang@gmail.com>
2015-07-05 23:47:16 -0400

    update tssh.py with options

--- tssh.py
+++ tssh.py
@@ -1,50 +1,105 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-# This doesn't mean threading in ssh. What it means is, every thread 
-# that this program spawns, creates a new subprocess to perform the 
-# system "ssh" command.
+import subprocess
+import threading
+import sys,os,shlex
+import argparse
 
-# Sample Usages
-# =============
-#
-# 1.
-# for i in server1 server2 server3 server4; 
-# do echo username@$i; 
-# done | ./tssh "echo 'Good night :P'; rm -rf /; "
-#
-# 2.
-# cat connection_list | ./tssh "ifconfig lo"
-#
-# 3.
-# cat server_list | while read server; 
-# do echo "username@$server"; 
-# done | ./tssh "top -b -n1 | head -n10" --noheader
+basecommand = "ssh -o StrictHostKeyChecking=no "
+header = "echo -n  \"%s: \";  "
 
-import sys,os,threading,shlex,subprocess
 
-basecommand = "ssh -o StrictHostKeyChecking=no"
-header = "echo \"%s says,\"; echo '=========================================='; "
+class Ssher(object):
+    #output = {'alive': [], 'dead': []} # Populated while we are running
+    hosts = [] # List of all hosts/servers in our input queue
+    cmd = ""
+    user = ""
 
-nohead=len(sys.argv)>2 and sys.argv[2]=='--noheader'
+    # How many ping process at the time.
+    thread_count = 4
 
-class Runner(threading.Thread):
-    def __init__(self, server, command):
-        super(Runner,self).__init__()
-        self.command = shlex.split(basecommand)
-        self.command += [server]
-        self.command += [ (header % server, "")[nohead] + str(command) +";"]
-    def run(self):
-        sout = subprocess.Popen(self.command, stdout=subprocess.PIPE).stdout
-        for i in sout:
+    # Lock object to keep track the threads in loops, where it can potentially be race conditions.
+    lock = threading.Lock()
+
+    def Runner(self, server):
+        # Use the system ping command with count of 1 and wait time of 1.
+        command = shlex.split(basecommand )
+        command += [ self.user + "@" + server]
+        command += [ header % server  + str(self.cmd) +";"]
+        output = subprocess.Popen(command, stdout=subprocess.PIPE).stdout
+        for i in output:
             print i.strip()
 
+    def pop_queue(self):
+        server = None
+
+        self.lock.acquire() # Grab or wait+grab the lock.
+
+        if self.hosts:
+            server = self.hosts.pop()
+
+        self.lock.release() # Release the lock, so another thread could grab it.
+
+        return server
+
+    def dequeue(self):
+        while True:
+            server = self.pop_queue()
+
+            if not server:
+                return None
+
+            #result = 'alive' if self.ping(server) else 'dead'
+            self.Runner(server)
+            #self.status[result].append(server)
+
+    def start(self):
+        threads = []
+
+        for i in range(self.thread_count):
+            # Create self.thread_count number of threads that together will
+            # cooperate removing every server in the list. Each thread will do the
+            # job as fast as it can.
+            t = threading.Thread(target=self.dequeue)
+            t.start()
+            threads.append(t)
+
+        # Wait until all the threads are done. .join() is blocking.
+        [ t.join() for t in threads ]
+
+
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Multithreading ssh')
+    parser.add_argument('command', action="store", type=str,
+                        help="Command to execute")
+    parser.add_argument('-t', action="store", dest="threads_no", type=int, default=6,
+                        help="Number of threads")
+    parser.add_argument('-u', action="store", dest="user", type=str, default="root",
+                        help="user name")
+    parser.add_argument('-k', action="store", dest="keyfile", type=str,
+                        help="key file")
+    
+    args = parser.parse_args()
+
+    servers = []
     while True:
         try:
-            server=raw_input()
+            server = raw_input()
+            print "server: %s" % server
+            servers.append( server )
         except EOFError:
             break
-        t=Runner(server=server, command=len(sys.argv)>1 and sys.argv[1] or "echo ")
-        t.start()
 
 
+
+    print servers
+    
+    tssh = Ssher()
+    tssh.thread_count = args.threads_no 
+    
+    tssh.hosts =  servers 
+    tssh.cmd = args.command
+    tssh.user = args.user
+
+    tssh.start()
